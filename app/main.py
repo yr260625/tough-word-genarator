e120842b63f01a346b31f0d97cfdeb1824585211
yr260625 <yr260625.learning@gmail.com>
2024-03-28 22:27:23 +0900

    [Test] CORS Allow

--- app/main.py
+++ app/main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
 from app.tough_word import ToughWord
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = [
+    "https://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def index():
     return {"Hello": "World"}
+
 
 @app.get("/tough-word")
 def generate_tough_word():
